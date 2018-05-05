e96e79389c82cff26be7a5dfebeb3249dab9c163
Berserkerere <39008457+Berserkerere@users.noreply.github.com>
2018-05-05 21:14:59 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+token = '598007373:AAG5Vwm6vGwKpM0MUFyocTzAP3LIei68Ql0'  # Токен бота
+database_name = 'music.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
